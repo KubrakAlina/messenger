e92220a798a59304061f54f46b9bbf4528a82f7f
Alina <alinakubikalex@gmail.com>
2025-12-01 18:33:03 +0100

    fix user search

--- src/components/SearchUser/SearchUser.tsx
+++ src/components/SearchUser/SearchUser.tsx
@@ -1,41 +1,48 @@
 'use client';
 
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useContext } from 'react';
 import { type UserData, ChatsData } from '@/api/types';
 import { fetchUser } from '@/api/user/fetchUser';
+import { fetchChats } from '@/api/chats/fetchChats';
 import { postChat } from '@/api/chats/postChats';
 import { useRouter } from 'next/navigation';
+import MessengerContext from "@/context/MessengerContext";
 import s from "./searchuser.module.scss"
 
 function UserSearch() {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState<UserData[]>([]);
   const [loading, setLoading] = useState(false);
-  const user = useRef<UserData[]>([]);
+  const [chatsList, setChatsList] = useState<ChatsData[]>([]);
   const users = useRef<UserData[]>([]);
   const router = useRouter();
+  const context = useContext(MessengerContext);
+  const { currentUser } = context;
 
   useEffect(() => {
-    //load all users
-    const loadUsers = async () => {
-      users.current = await fetchUser();
-      return users;
+    if (currentUser) {
+      const loadUsers = async () => {
+        users.current = await fetchUser();
+        return users;
+      }
+
+      const loadChats = async () => {
+        const chats = await fetchChats();
+        setChatsList(chats);
+      }
+      loadUsers();
+      loadChats()
     }
-    loadUsers();
+    else { return }
 
-    //find logged user
-    const storedUser = sessionStorage.getItem("user");
-    if (storedUser) {
-      user.current = JSON.parse(storedUser);
-    } else { return }
-
-  }, [])
+  }, [currentUser])
 
   useEffect(() => {
     const delayDebounce = setTimeout(() => {
       if (query.trim().length > 0) {
-        const finded = users.current.filter((user => user.username.includes(query.toLowerCase())))
-        setResults(() => [...finded])
+        const finded = users.current.filter((user => user.username.includes(query.toLowerCase())));
+        const filtered = finded.filter((findedUser => findedUser.id !== currentUser?.id))
+        setResults(() => [...filtered])
         if (results) {
           setLoading(false);
         }
@@ -48,19 +55,20 @@ function UserSearch() {
     return () => clearTimeout(delayDebounce);
   }, [query]);
 
+  async function findExistentChat(userTo: UserData) {
+    if (currentUser) {
+      const filtered = chatsList.filter((chat) => chat.user1.id === userTo.id || chat.user2.id === userTo.id)[0];
+      return filtered;
+    }
+  }
+
   //create new chat
   async function createNewChat(userTo: UserData) {
-    const storedUser = sessionStorage.getItem("user");
-    if (!storedUser) {
-      //add modal
-      console.log("no stored user");
-      return;
+    if (currentUser) {
+      const newChat: ChatsData = { user1: { id: currentUser.id, name: currentUser.name }, user2: { id: userTo.id, name: userTo.name } };
+      const result = await postChat(newChat);
+      return result;
     }
-
-    const user = JSON.parse(storedUser) as UserData;
-    const newChat: ChatsData = { user1: { id: user.id, name: user.name }, user2: { id: userTo.id, name: userTo.name } };
-    const result = await postChat(newChat);
-    return result;
   }
 
   //go to new chat
@@ -69,10 +77,18 @@ function UserSearch() {
   }
 
   async function handleClick(user: UserData) {
-    const chat = await createNewChat(user);
+    let chat;
+    try {
+      chat = await findExistentChat(user);
+      console.log(chat);
+    } catch (error) {
+      chat = await createNewChat(user);
+    }
 
-    if (chat?.id) {
-      goToChat(chat.id);
+    finally {
+      if (chat?.id) {
+        goToChat(chat.id);
+      }
     }
   }
 
